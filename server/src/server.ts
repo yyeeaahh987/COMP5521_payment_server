8d593b5132c3b330435ee17ede70d932151dded4
gap <tyehcwc402004@gmail.com>
2023-04-07 01:05:27 +0800

    init node express server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,13 @@
-import express, {Express, Request, Response} from 'express';
+import express, { Express, Request, Response } from 'express';
 
 const app: Express = express();
 const port = 8080;
 
-app.get('/', (req: Request, res: Response)=>{
-    console.log(`this is typescript123`)
-    res.send('Hello, this is Express + TypeScript');
+app.get('/', (req: Request, res: Response) => {
+  console.log(`this is typescript121231233`)
+  res.send('Hello, this is Express + TypeScript');
 });
 
-app.listen(port, ()=> {
-console.log(`[Server]: I am running at http://localhost:${port}`);
+app.listen(port, () => {
+  console.log(`[Server]: I am running at http://localhost:${port}`);
 });
